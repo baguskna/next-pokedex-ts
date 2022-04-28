902481540e55b887e02d1649fc5b6ef4a6241060
Bagus Kurnia <bagus@alore.io>
2022-04-28 20:26:24 +0700

    Add style on detail page

--- pages/detail/[pokemonName].tsx
+++ pages/detail/[pokemonName].tsx
@@ -1,16 +1,23 @@
 import { NextPage } from "next";
 import { gql } from "@apollo/client";
 import Image from "next/image";
+import { css } from "@emotion/react";
 
 import { client } from "../../lib/apollo-client";
 import { PokemonProps } from "../../lib/interfaces";
 import { H2 } from "../../lib/pokedex-design-system";
 
+const detailContainer = css`
+  max-width: 904px;
+  margin: 0 auto;
+  padding-top: 90px;
+`;
+
 const DetailPage: NextPage<PokemonProps> = (props) => {
   const { name, sprites } = props.pokemon;
 
   return (
-    <div>
+    <div css={detailContainer}>
       <H2>{name}</H2>
       <Image src={sprites.front_default} width={175} height={175} alt={name} />
     </div>
